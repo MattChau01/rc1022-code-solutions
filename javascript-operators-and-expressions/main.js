b22f7714ee5d744b5ceae4796f62c1b4d09428f0
Matthew Chau <matthewwchau@gmail.com>
2022-10-17 18:24:18 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,45 @@
+var width = 7;
+var height = 9;
+var area = width * height;
+
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+var bill = 61;
+var payment = 72;
+var change = payment - bill;
+
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
+
+var quizzes = 80;
+var midterm = 90;
+var final = 90;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
+
+var firstName = 'Matthew';
+var lastName = 'Chau';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+var pH = 9;
+var isAcidic = pH < 7;
+
+console.log('value of isAcific: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+var headCount = 300;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+
+console.log('value of motto: ', motto);
+console.log('typeof motto', typeof motto);
