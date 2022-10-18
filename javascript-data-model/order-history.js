f9c01fde1d3fb3d6aff069dad97abf94bf70802e
Matthew Chau <matthewwchau@gmail.com>
2022-10-18 16:32:59 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,36 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    totalPrice: '$34.00',
+    orderNum: '114-3941689-8772232',
+    title: 'JavaScript for impatient programmers',
+    author: 'Dr. Axel Rauschmayer'
+  },
+  {
+    orderPlaced: 'July 19,2020',
+    totalPrice: '$44.53',
+    orderNum: '113-9984268-1280257',
+    title: 'The Timeless Way of Building',
+    author: 'Christopher Alexander'
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    totalPrice: '$17.22',
+    orderNum: '114-2875557-9059409',
+    title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and vibration features. No Driver and No Lag-Gamecube Adapter'
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    totalPrice: '$138.93',
+    orderNum: '113-2883177-2648248',
+    title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)'
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    totalPrice: '$138.93',
+    orderNum: '113-2883177-2648248',
+    title: 'The Art of SQL'
+  }
+];
+
+console.log('The order history of the first order is: ', orderHistory[0]);
