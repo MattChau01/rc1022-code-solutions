b461d65c66185e20a82e561e9483f64e3c386283
Matthew Chau <matthewwchau@gmail.com>
2022-10-21 22:24:49 +0000

    Completed array-manipulation-1

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,11 @@
+/* exported head */
+
+// Pseudo Code://
+// Define a new function with the name head with one parameter (array)
+// Assign the first index of an element to a new variable
+// Return the result of the expression
+
+function head(array) {
+  var first = array[0];
+  return first;
+}
---
+++ array-manipulation-1/src/last.js
@@ -0,0 +1,10 @@
+/* exported last */
+// Pseudocode:/
+// Define a new function with the name last with one parameter (array)
+// Grab the last element of the array and assign it to a new variable lastElement
+// return the expression of the array at the index of the last element
+
+function last(array) {
+  var lastElement = array.length - 1;
+  return array[lastElement];
+}
