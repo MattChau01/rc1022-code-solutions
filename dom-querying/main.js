39082437e8b1e5dc9e07f41f94a13dc334d57ca7
Matthew Chau <matthewwchau@gmail.com>
2022-10-24 17:03:58 +0000

    Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,16 @@
+console.log('hello, world');
+
+var domH1 = document.querySelector('h1');
+console.log(domH1);
+console.dir(domH1);
+
+var domEx = document.querySelector('#explanation');
+console.log(domEx);
+console.dir(domEx);
+
+var domHi = document.querySelector('.hint');
+console.log(domHi);
+console.dir(domHi);
+
+console.log(document.querySelectorAll('p'));
+console.log(document.querySelectorAll('.example-link'));
