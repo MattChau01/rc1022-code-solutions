43a81d8058a0374c7756046b1d1b56092ad249f2
Matthew Chau <matthewwchau@gmail.com>
2022-11-30 08:14:08 +0000

    javascript-typing-tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,6 +1,17 @@
-function typer(event) {
-
-}
 
 var $typer = document.getElementById('typer');
 $typer.addEventListener('keydown', typer);
+
+// var letter = document.querySelectorAll('.default');
+// console.log(letter);
+
+function typer(key) {
+  // console.log(key.value);
+  // for (var i = 0; i < letter.length; i++) {
+  //   if (key.value === letter[i]) {
+  //     console.log('test');
+  //   }
+  // }
+
+  // console.log(letter[0]);
+}
