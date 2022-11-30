2119f515abcf3009a6f7e364e4f69b850e9c5a6e
Matthew Chau <matthewwchau@gmail.com>
2022-11-30 04:14:50 +0000

    Completed string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,7 +1,7 @@
 /* exported titleCase */
 
 function titleCase(title) {
-  var conjunctions = ['and', 'or', 'nor', 'but', 'for', 'in', 'of', 'the'];
+  var conjunctions = ['and', 'or', 'nor', 'but', 'for', 'in', 'of', 'the', 'on', 'to'];
   var lowerCase = title.toLowerCase();
   lowerCase = lowerCase.replace('javascript', 'JavaScript');
   lowerCase = lowerCase.replace('api', 'API');
@@ -9,11 +9,14 @@ function titleCase(title) {
 
   breakDown[0] = breakDown[0].charAt(0).toUpperCase() + breakDown[0].slice(1);
 
+  // console.log(breakDown[0]);
+
   for (var i = 0; i < breakDown.length; i++) {
     if (!(conjunctions.includes(breakDown[i]))) {
       var firstLetter = breakDown[i].charAt(0).toUpperCase();
       var rest = breakDown[i].slice(1);
       breakDown[i] = firstLetter + rest;
+      // console.log(breakDown[i]);
     }
     // colon
 
@@ -23,7 +26,18 @@ function titleCase(title) {
       }
     }
 
-  }
-  // console.log(breakDown);
+    for (var l = 0; l < breakDown[i].length; l++) {
+      if (breakDown[i][l] === ':') {
+        // console.log(breakDown[i + 1]);
+        // var afterColon = breakDown[i + 1];
+        // console.log(afterColon.charAt(0).toUpperCase() + afterColon.slice(1, l));
+        breakDown[i + 1] = breakDown[i + 1].charAt(0).toUpperCase() + breakDown[i + 1].slice(1, l);
+        // console.log(breakDown[i + 1]);
+      }
+    }
 
+  }
+  var final = breakDown.join(' ');
+  // console.log(final);
+  return final;
 }
