517d707fcf5b1c934f0b97b3f5e96336512474e4
Matthew Chau <matthewwchau@gmail.com>
2022-12-01 05:16:19 +0000

    Completed array-manipulation-3

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -8,38 +8,6 @@
 // If they do not match, return false
 
 function equal(first, second) {
-  // console.log(first.length);
-  // console.log(second.length);
-
-  // for (var i = 0; i < first.length; i++) {
-  //   var firstArray = first[i];
-  //   // console.log(firstArray);
-  // }
-
-  // for (var j = 0; j < second.length; j++) {
-  //   var secondArray = second[j];
-  // }
-
-  // if ((first === [1, 2, 3, 4, 5]) && (second === [1, 2, 3, 3, 5])) {
-  //   return false;
-  // }
-
-  // // if (first.length !== second.length) {
-  // //   return false;
-  // // }
-
-  // // HERE
-
-  // if (first.length === second.length) {
-  //   if (firstArray === secondArray) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
-
-  // Solution:
-
   if (first.length !== second.length) {
     return false;
   }
