ba10fdfadb4ed1020e725bbc7e28faaef2a75efd
Matthew Chau <matthewwchau@gmail.com>
2022-12-06 23:09:03 +0000

    Updated express-post-json

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -3,17 +3,17 @@ const app = express();
 
 let nextId = 1;
 
-let grades =
+const grades =
   {
   };
 
 app.listen(3000, () => {
-  // console.log('Listening on port 3000!');
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
 });
 
-const array = [];
-
 app.get('/api/grades', (req, res) => {
+  const array = [];
   for (const student in grades) {
     array.push(grades[student]);
   }
@@ -24,16 +24,13 @@ const middleWare = express.json();
 
 app.use(middleWare);
 
-app.post('/api/grades/', (req, res) => {
+app.post('/api/grades', (req, res) => {
   // console.log('req.body: ', req.body);
 
   const reqBody = req.body;
-  grades = {
-    reqBody
-  };
-
-  grades.reqBody.id = nextId;
+  reqBody.id = nextId;
   // console.log('grades: ', grades);
+  grades[nextId] = reqBody;
   nextId++;
-  res.sendStatus(201).json();
+  res.status(201).json(reqBody);
 });
